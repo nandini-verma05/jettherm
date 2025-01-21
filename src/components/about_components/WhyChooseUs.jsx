06182570f098c658132c77e373fe1c7a3faf93d5
nandini-verma05 <nandinisinghverma123@gmail.com>
2025-01-21 20:53:55 +0530

    .

--- src/components/about_components/WhyChooseUs.jsx
+++ src/components/about_components/WhyChooseUs.jsx
@@ -253,9 +253,9 @@ const WhyChooseUs = ({windowWidth}) => {
 
   return (
     <div className="overflow-hidden min-h-screen  py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-7xl mx-auto">
+         
      {windowWidth < 640 ? <MobileLayout /> : <CircularFeatureLayout features={features} />}
-    </div>
+   
     </div>
   );
 };
